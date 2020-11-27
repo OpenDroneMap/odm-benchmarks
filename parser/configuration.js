a168b5a2d1c471c65d262f57067f21df1e3044f0
Corey Snipes <corey@coreysnipes.com>
2020-11-27 13:23:16 -0500

    Relocated parser output

--- parser/configuration.js
+++ parser/configuration.js
@@ -10,34 +10,8 @@
 module.exports = (config) => {
   config.inFileDatasets = '../data/datasets.csv';
   config.inFileBenchmarks = '../data/benchmarks.csv';  
-  config.outFileByDataset = 'out/by-dataset.txt';  
-  config.outFileByVersion = 'out/by-version.txt';  
+  config.outFileByDataset = '../data-parsed/by-dataset.txt';  
+  config.outFileByVersion = '../data-parsed/by-version.txt';  
+  // config.outFileByDataset = 'out/by-dataset.txt';  
+  // config.outFileByVersion = 'out/by-version.txt';  
 }
-
-//COLUMN: INCLUDE_IN_SUMMARY
-// const columns = {
-//   "ID": false,
-//   "DATASET": true,
-//   "PROCESSING_TIME": true,
-//   "PROCESSING_SUCCESS": true,
-//   "ERROR_TYPE": true,
-//   "RAM_SIZE": true,
-//   "RAM_CLOCK_SPEED": false,
-//   "CPU_TYPE": true,
-//   "CPU_CLOCK_SPEED": false,
-//   "CPU_NUM_CORES": false,
-//   "STORAGE_TYPE": false,
-//   "OS": true,
-//   "VM_TYPE": true,
-//   "ODM_VERSION": true,
-//   "ODM_CLUSTER": false,
-//   "CONFIG_NAME": true,
-//   "CONFIG_RESIZE": true,
-//   "CONFIG_OTHER": true,
-//   "TEST_DATE": true,
-//   "TEST_BY": true,
-//   "INCLUDE_IN_SUMMARY": false,
-//   "NOTES": false
-// };
-
-// -----------------------------------------------------------------------------
