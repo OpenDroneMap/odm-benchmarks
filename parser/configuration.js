096833bf028be3d2f25d9accd43b00949facc1aa
Corey Snipes <corey@coreysnipes.com>
2020-11-27 07:57:54 -0500

    Move column spec to configuration.js

---
+++ parser/configuration.js
@@ -0,0 +1,37 @@
+/** ----------------------------------------------------------------------------
+ * configuration.js
+ * 
+ * For specifying certain operating parameters of the benchmark data processor.
+ * -----------------------------------------------------------------------------
+ */
+
+
+"use strict";
+
+//COLUMN: INCLUDE_IN_SUMMARY
+const columns = {
+  "ID": false,
+  "DATASET": true,
+  "PROCESSING_TIME": true,
+  "PROCESSING_SUCCESS": true,
+  "ERROR_TYPE": true,
+  "RAM_SIZE": true,
+  "RAM_CLOCK_SPEED": false,
+  "CPU_TYPE": true,
+  "CPU_CLOCK_SPEED": false,
+  "CPU_NUM_CORES": false,
+  "STORAGE_TYPE": false,
+  "OS": true,
+  "VM_TYPE": true,
+  "ODM_VERSION": true,
+  "ODM_CLUSTER": false,
+  "CONFIG_NAME": true,
+  "CONFIG_RESIZE": true,
+  "CONFIG_OTHER": true,
+  "TEST_DATE": true,
+  "TEST_BY": true,
+  "INCLUDE_IN_SUMMARY": false,
+  "NOTES": false
+};
+
+// -----------------------------------------------------------------------------
